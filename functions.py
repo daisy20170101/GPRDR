8b45501cc9aed91c3d5fe553ec6daefd37243960
DL <duoli@VenusMac.local>
2020-03-23 14:53:07 +0100

    update functions

--- functions.py
+++ functions.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# define functions used in GPR model based on GPRDI (Maurizio)
+
 import numpy as np
 
-def CalcVp(strain,strain0, D, dt):
-    
+def CalcVp(strain,strain0, D, dt): # based on Horrendorf et al., 2019
+    """
+    % variables:
+    % D: thickness of fault zone
+    % dt: delta t
+    % strain0, strain: strain tensor of this and last time step, respectively
+    """    
     strainR = (strain-strain0) / dt;
     
     RR = 0.5*((strainR[1,1]-strainR[2,2])**2+(strainR[1,1]-strainR[0,0])**2+(strainR[0,0]-strainR[2,2])**2) + 3*(strainR[1,2]**2+ strainR[0,2]**2+ strainR[0,1]**2);
@@ -15,7 +22,12 @@ def CalcVp(strain,strain0, D, dt):
 
 
 def GetXi(c0,t,dt,mu1,mu2,ti,te):  # eq 59
-
+    """
+    % variables:
+    % mu1,mu2: shear modulus
+    % ti, te, t, dt: start and ending time of damage, time and delta t
+    % c0: damage coeff. of last time step
+    """
     # get damage coefficient c
 	if (t<ti or t>te):
 		dcdt = 0;
@@ -28,17 +40,23 @@ def GetXi(c0,t,dt,mu1,mu2,ti,te):  # eq 59
 	return c;
 
 def second(sigma):
+    """
+    % variables:
+    % sigma: stress tensor
+    """
 # second invariant
 
     yy = np.sqrt(0.5*((sigma[1,1]-sigma[2,2])**2+(sigma[1,1]-sigma[0,0])**2+(sigma[0,0]-sigma[2,2])**2) + 3*(sigma[1,2]**2+sigma[0,2]**2+sigma[0,1]**2)) ;
     return yy;
       
-def  StrainToStress(strain,L,M):
-
+def  StrainToStress(strain,L,M):  # convert strain to stress
+    """
+    % variables:
+    % L, M: averaged lame's and shear modulus
+    """
     # calc stress tensor from strain tensor
     sigma = np.zeros([3,3]);
     ss = np.matrix(strain);
-#     print(ss);
 
     sigma[1,1]=(L+2/3*M)*ss.trace() + 2*M*(strain[1,1]-ss.trace()/3);
     sigma[2,2]=(L+2/3*M)*ss.trace() + 2*M*(strain[2,2]-ss.trace()/3);
